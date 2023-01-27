9ceb616694a96d6f1d553c4e222f658d73aeba00
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-01-28 00:27:43 +0300

    lr 1 task 2 var 3 meth 1

---
+++ LAB1/TASK2/methods.rb
@@ -0,0 +1,29 @@
+# вариант 3
+# метод 1, найти максимальный простой делитель числа
+
+def simple(num)
+    return false if num <= 1
+    2.upto(Math.sqrt(num).to_i).each  {|i| return false if num % i == 0}
+    # Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+    true
+end
+
+def max_and_simple_del(num)
+    max = -1
+    for x in 0..num        
+        if simple(x) == true and x > max and num % x == 0
+            max = x
+        end 
+    end
+    if max == -1
+        return "У числа нет простого делителя"
+    else
+        return max
+    end
+end
+
+puts "Введите число для поиска его максимального простого делителя"
+n = gets.to_i
+puts sprintf("Введённое число, %s", n)
+puts sprintf("Результат для введённого числа: %s", max_and_simple_del(n))
+
