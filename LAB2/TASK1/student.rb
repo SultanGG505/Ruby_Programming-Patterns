afda8f3e778fcfcf74f15b8fe59b281f7880efba
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-05 22:53:22 +0300

    surname

--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -1,11 +1,11 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :github
 
-  def initialize(last_name:, first_name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
+  def initialize(last_name:, first_name:, surname:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
-    @patronymic = patronymic
+    @surname = @surname
     @phone = phone
     @telegram = telegram
     @email = email
@@ -13,7 +13,7 @@ class Student
   end
 
   def to_s
-    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@patronymic}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
+    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@surname}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
   end
 end
 
