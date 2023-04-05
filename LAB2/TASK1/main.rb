0d8ec95a6054cc4ddf0041f91ca245409f4a9498
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-05 23:25:34 +0300

    PHONE_REGEX

--- LAB2/TASK1/main.rb
+++ LAB2/TASK1/main.rb
@@ -1,13 +1,13 @@
 require_relative 'student.rb'
 
 # Создаем несколько объектов класса Student
-student1 = Student.new(last_name: 'Гослинг', first_name: 'Райан', surname: 'Драйвович', id: '1', phone: '112', telegram: '@drive', email: 'endOfDrive@yandex.ru', github: 'driver')
-student2 = Student.new(last_name: 'Гордов', first_name: 'Султан', surname: 'Николаевич', id: '2', phone: '89184741586', telegram: '@sultan', email: 'sultangordovg@gmail.com', github: 'sultanGG505')
-student3 = Student.new(last_name: 'Иванов', first_name: 'Иван', surname: 'Иванович', id: '3')
+#student1 = Student.new(last_name: 'Гослинг', first_name: 'Райан', patronymic: 'Драйвович', id: '1', phone: '112', telegram: '@drive', email: 'endOfDrive@yandex.ru', github: 'driver')
+student2 = Student.new(last_name: 'Гордов', first_name: 'Султан', patronymic: 'Николаевич', id: '2', phone: '89184741586', telegram: '@sultan', email: 'sultangordovg@gmail.com', github: 'sultanGG505')
+student3 = Student.new(last_name: 'Иванов', first_name: 'Иван', patronymic: 'Иванович', id: '3')
 
 
 # Выводим информацию о каждом студенте
-puts "Студент 1->", student1.to_s
+# puts "Студент 1->", student1.to_s
 
 puts "Студент 2->", student2.to_s
 
--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -1,19 +1,27 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :github
 
-  def initialize(last_name:, first_name:, surname:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
+  PHONE_REGEX = /\A(\+7|8)\d{10}\z/
+
+  def initialize(last_name:, first_name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
-    @surname = @surname
-    @phone = phone
+    @patronymic = patronymic
+    self.phone = phone
     @telegram = telegram
     @email = email
     @github = github
   end
 
+  def phone=(value)
+    if value && !value.match(PHONE_REGEX)
+      raise ArgumentError.new("Неправильный формат номера телефона: #{value}")
+    end
+    @phone = value
+  end
+
   def to_s
-    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@surname}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
+    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@patronymic}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
   end
 end
-
