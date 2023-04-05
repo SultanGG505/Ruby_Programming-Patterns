be5d41dccab1fd267e954512ad7592648c486a98
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-06 00:27:52 +0300

    regexes

--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -2,13 +2,15 @@ class Student
   attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :github
 
   PHONE_REGEX = /\A(\+7|8)\d{10}\z/
+  TELEGRAM_REGEX = /^@[A-Za-z0-9\-_]+$/
+  EMAIL_REGEX = /^[A-Za-z0-9\-_]+@[A-Za-z]+\.([A-Za-z]+\.)*[A-Za-z]+$/
 
   def initialize(last_name:, first_name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
     @patronymic = patronymic
-    self.phone = phone
+    @phone = phone
     @telegram = telegram
     @email = email
     @github = github
@@ -21,6 +23,20 @@ class Student
     @phone = value
   end
 
+
+  def telegram=(value)
+    if value && !value.match(TELEGRAM_REGEX)
+      raise ArgumentError.new("Неправильный формат телеграма: #{value}")
+    end
+    @telegram = value
+  end
+
+  def email=(value)
+    if value && !value.match(EMAIL_REGEX)
+      raise ArgumentError.new("Неправильный формат почты: #{value}")
+    end
+    @email = value
+  end
   def to_s
     "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@patronymic}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
   end
