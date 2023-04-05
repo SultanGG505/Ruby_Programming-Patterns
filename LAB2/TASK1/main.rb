c9b40217f9b222d4e497fc23d70bd20573fcaa78
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-05 22:45:16 +0300

    named fields
    Обеспечена в конструкторе
    объектов с любой необязательных полей.

--- LAB2/TASK1/main.rb
+++ LAB2/TASK1/main.rb
@@ -1,9 +1,10 @@
 require_relative 'student.rb'
 
 # Создаем несколько объектов класса Student
-student1 = Student.new('Гослинг', 'Райан', 'Драйвович', '1', '112', '@drive', 'endOfDrive@yandex.ru', 'driver')
-student2 = Student.new('Гордов', 'Султан', 'Николаевич', '2', '89184741586', '@sultan', 'sultangordovg@gmail.com', 'sultanGG505')
-student3 = Student.new('Иванов', 'Иван', 'Иванович', '3')
+student1 = Student.new(last_name: 'Гослинг', first_name: 'Райан', patronymic: 'Драйвович', id: '1', phone: '112', telegram: '@drive', email: 'endOfDrive@yandex.ru', github: 'driver')
+student2 = Student.new(last_name: 'Гордов', first_name: 'Султан', patronymic: 'Николаевич', id: '2', phone: '89184741586', telegram: '@sultan', email: 'sultangordovg@gmail.com', github: 'sultanGG505')
+student3 = Student.new(last_name: 'Иванов', first_name: 'Иван', patronymic: 'Иванович', id: '3')
+
 
 # Выводим информацию о каждом студенте
 puts "Студент 1->", student1.to_s
--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -1,7 +1,7 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :github
 
-  def initialize(last_name, first_name, patronymic, id=nil, phone=nil, telegram=nil, email=nil, github=nil)
+  def initialize(last_name:, first_name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
@@ -11,7 +11,9 @@ class Student
     @email = email
     @github = github
   end
+
   def to_s
     "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@patronymic}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
   end
 end
+
