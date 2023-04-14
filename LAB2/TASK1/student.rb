985e620076d2ca1f00fd965ae6be72c3577b5abb
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-14 23:07:12 +0300

    again LR2/task2/ex_1

--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -1,55 +1,19 @@
+
 class Student
-  attr_reader :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :github
+  # стандартные геттеры и сеттеры для класса
+  attr_accessor :id, :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
 
-  PHONE_REGEX = /\A(\+7|8)\d{10}\z/
-  TELEGRAM_REGEX = /^@[A-Za-z0-9\-_]+$/
-  EMAIL_REGEX = /^[A-Za-z0-9\-_]+@[A-Za-z]+\.([A-Za-z]+\.)*[A-Za-z]+$/
-
-  def initialize(last_name:, first_name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, github: nil)
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @patronymic = patronymic
-    self.phone = phone
-    self.telegram = telegram
-    self.email = email
-    @github = github
+  # конструктор
+  def initialize(last_name, first_name, paternal_name, options = {})
+    self.last_name = last_name
+    self.first_name = first_name
+    self.paternal_name = paternal_name
+    self.id = options[:id]
+    self.phone = options[:phone]
+    self.git = options[:git]
+    self.telegram = options[:telegram]
+    self.email = options[:email]
   end
 
-  def phone=(value)
-    raise ArgumentError, "Неправильный формат номера телефона: #{value}" if value && !value.match(PHONE_REGEX)
 
-    @phone = value
-  end
-
-  def telegram=(value)
-    raise ArgumentError, "Неправильный формат телеграма: #{value}" if value && !value.match(TELEGRAM_REGEX)
-
-    @telegram = value
-  end
-
-  def email=(value)
-    raise ArgumentError, "Неправильный формат почты: #{value}" if value && !value.match(EMAIL_REGEX)
-
-    @email = value
-  end
-
-  def to_s
-    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@patronymic}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nGitHub: #{@github}"
-  end
-
-  def contact
-    return @contact = "phone=#{phone}" unless phone.nil?
-    return @contact = "telegram=#{telegram}" unless telegram.nil?
-    return @contact = "email=#{email}" unless email.nil?
-
-    nil
-  end
-
-  def set_contacts(phone: nil, telegram: nil, email: nil)
-    self.phone = phone if phone
-    self.telegram = telegram if telegram
-    self.email = email if email
-    @contact = contact
-  end
 end
