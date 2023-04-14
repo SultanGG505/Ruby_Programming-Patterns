6e303aa6b687b89ed91094d3e2725340b320be11
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-14 23:25:01 +0300

    task1 valids
    поменял аксессоры на ридеры и сеттеры, сделал нормальные валидаторы вообще для всего, раньше было не для всего

--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -1,11 +1,26 @@
-
 class Student
   # стандартные геттеры и сеттеры для класса
-  attr_accessor :id, :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
+  attr_accessor :id
+  attr_reader :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
 
   #валидатор мобильного
   def self.valid_phone?(phone)
     phone.match(/^\+?[7,8] ?\(?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/)
+  end
+
+  #валидатор имени
+  def self.valid_name?(name)
+    name.match(/^[А-Я][а-я]+$/)
+  end
+
+  #валидатор данных для аккаунта
+  def self.valid_account?(account)
+    account.match(/^@[A-Za-z0-9\-_]+$/)
+  end
+
+  #валидатор почты
+  def self.valid_email?(email)
+    email.match(/^[A-Za-z0-9\-_]+@[A-Za-z]+\.([A-Za-z]+\.)*[A-Za-z]+$/)
   end
 
   # конструктор
@@ -20,12 +35,48 @@ class Student
     self.email = options[:email]
   end
 
-
   def phone=(phone)
     raise ArgumentError, "Incorrect value: phone=#{phone}!" if !phone.nil? && !Student.valid_phone?(phone)
 
     @phone = phone
   end
+
+  def first_name=(first_name)
+    raise ArgumentError, "Incorrect value: first_name=#{first_name}!" if !first_name.nil? && !Student.valid_name?(first_name)
+
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    raise ArgumentError, "Incorrect value: last_name=#{last_name}!" if !last_name.nil? && !Student.valid_name?(last_name)
+
+    @last_name = last_name
+  end
+
+  def paternal_name=(paternal_name)
+    raise ArgumentError, "Incorrect value: paternal_name=#{paternal_name}!" if !paternal_name.nil? && !Student.valid_name?(paternal_name)
+
+    @paternal_name = paternal_name
+  end
+
+  def git=(git)
+    raise ArgumentError, "Incorrect value: git=#{git}!" if !git.nil? && !Student.valid_account?(git)
+
+    @git = git
+  end
+
+  def telegram=(telegram)
+    raise ArgumentError, "Incorrect value: telegram=#{telegram}!" if !telegram.nil? && !Student.valid_account?(telegram)
+
+    @telegram = telegram
+  end
+
+  def email=(email)
+    raise ArgumentError, "Incorrect value: email=#{email}!" if !email.nil? && !Student.valid_email?(email)
+
+    @email = email
+  end
+
   def to_s
     result = "#{last_name} #{first_name} #{paternal_name}"
     result += " id=#{id}" unless id.nil?
@@ -35,6 +86,5 @@ class Student
     result += " email=#{email}" unless email.nil?
     result
   end
-
 
 end
