a5d941ba740b71b2b9a8f35800a363ec7c573ce6
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-27 19:44:56 +0300

    fix and again task3
    починил один файл, в который был записан уже занесённый в отдельный класс файл и начал переделывать задачу 3 с учётом ошибок

--- LAB2/TASK1/examples.rb
+++ LAB2/TASK1/examples.rb
@@ -1,5 +1,3 @@
-require_relative 'data_table.rb'
-require_relative 'data_list.rb'
 require_relative 'student.rb'
 require_relative 'student_short'
 require_relative 'person'
@@ -19,10 +17,3 @@ require_relative 'person'
 # puts table.num_columns # => 3
 # puts table.num_rows # => 3
 
-data_list = Data_list.new
-data_list.add(Student.new(1, "Doe", "John", "Adam", git: "johndoe", email: "johndoe@example.com"))
-data_list.add(Student.new(2, "Smith", "Jane", "Marie", phone: "123-456-7890", telegram: "@janesmith"))
-data_list.select(1)
-puts data_list.get_selected #=> [1]
-puts data_list.get_names #=> ["last_name", "first_name", "paternal_name", "phone", "git", "telegram", "email"]
-puts data_list.get_data #=> [["№", "last_name", "first_name", "paternal_name", "phone", "git", "telegram", "email"], [1, "Doe", "John", "Adam", nil, "johndoe", nil, "johndoe@example.com"], [2, "Smith", "Jane", "Marie", "123-456-7890", nil, "@janesmith", nil]]
--- LAB2/TASK1/student.rb
+++ LAB2/TASK1/student.rb
@@ -56,24 +56,4 @@ class Student < Person
   attr_writer :phone, :git, :telegram, :email
 end
 
-class StudentShort < Person
-  attr_reader :git, :contact
 
-  def initialize(id, name_string, git: nil, contact: nil)
-    last_name, first_name, paternal_name = name_string.split(" ")
-    super(id, last_name, first_name, paternal_name)
-    @git = git
-    @contact = contact
-  end
-
-  def to_s
-    result = super
-    result += " git=#{git}" unless git.nil?
-    result += " contact=#{contact}" unless contact.nil?
-    result
-  end
-
-  def getInfo
-    "#{short_name}, #{contact}"
-  end
-end
