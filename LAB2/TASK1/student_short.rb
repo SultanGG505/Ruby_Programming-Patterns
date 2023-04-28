f4906b3f9b9c845a4ece496a8e1f6468063f1cad
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-28 11:51:59 +0300

    refactor
    теперь нету лишнего класса person

--- LAB2/TASK1/student_short.rb
+++ LAB2/TASK1/student_short.rb
@@ -1,23 +1,41 @@
-require_relative 'person.rb'
-
-class StudentShort < Person
-  attr_reader :git, :contact
-
-  def initialize(id, name_string, git: nil, contact: nil)
-    last_name, first_name, paternal_name = name_string.split(" ")
-    super(id, last_name, first_name, paternal_name)
-    @git = git
-    @contact = contact
+class StudentShort
+  # стандартные геттеры и сеттеры для класса
+  attr_reader :id, :git, :contact, :last_name, :initials
+  #конструктор, принимающий аргументы класса student
+  def initialize(student)
+    @id = student.id
+    @last_name = student.last_name
+    @initials = "#{student.first_name[0]}. #{student.paternal_name[0]}."
+    @git = student.git unless student.git.nil?
+    @contact = student.contact
   end
 
+  #фамилия и инициалы
+  def last_name_and_initials
+    "#{last_name} #{initials}"
+  end
+
+  # метод возвращающий представление объекта в виде строки
   def to_s
-    result = super
-    result += " git=#{git}" unless git.nil?
-    result += " contact=#{contact}" unless contact.nil?
+    result = last_name_and_initials
+    result += " id= #{id} " unless id.nil?
+    result += contact unless contact.nil?
     result
   end
 
-  def getInfo
-    "#{short_name}, #{contact}"
+  # метод проверяющий наличие гита
+  def git?
+    !git.nil?
   end
+
+  # метод проверяющий наличие контакта
+  def contact?
+    !contact.nil?
+  end
+
+  def validate
+    git? && contact?
+  end
+
 end
+
