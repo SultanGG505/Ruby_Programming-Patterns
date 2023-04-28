db8fd63acf3a7cca6cbca65c37ee208a06e38e20
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-04-29 02:49:04 +0300

    Create student_list_yaml.rb

---
+++ LAB2/lab2_fold/student_list_yaml.rb
@@ -0,0 +1,17 @@
+require_relative 'student_files_base'
+require 'yaml'
+
+class StudentListYAML < StudentListBase
+  public_class_method :new
+
+  protected
+
+  def str_to_list(str)
+    YAML.safe_load(str).map { |h| h.transform_keys(&:to_sym) }
+  end
+
+  def list_to_str(list)
+    YAML.dump(list.map { |h| h.transform_keys(&:to_s) })
+  end
+end
+
