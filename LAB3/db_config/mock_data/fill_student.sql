8664ff521a74634d243df6ff61a445f73b66f727
Sultan <sultangordovg@gmail.com>
2023-05-06 08:57:02 +0300

    подключена БД + записаны данные из SQL

---
+++ LAB3/db_config/mock_data/fill_student.sql
@@ -0,0 +1,6 @@
+insert into student(last_name, first_name, father_name, phone, telegram, email, git) values
+    ('Иванова', 'Анна', 'Сергеевна', '79634791028', NULL, 'sbeu@mail.ru', NULL),
+    ('Петров', 'Дмитрий ', 'Александрович', NULL, 'sultangg505', NULL, 'pupupu'),
+    ('Сидорова', 'Екатерина', 'Владимировна', '79181237654', 'sultangg506', 'gordov@gmail.com', NULL),
+    ('Кузнецов', 'Артём', 'Олегович', '79260123456', 'sultangg507', NULL, 'abimba'),
+    ('Михайлова', 'Наталья', 'Игоревна', '79763214567', 'sultangg508', 'text@mail.ru', 'mygit');
