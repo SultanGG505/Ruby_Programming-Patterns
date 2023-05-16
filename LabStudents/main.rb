d6c69462bf57dd854fe551c1f77904d533415bee
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-05-16 12:11:42 +0300

    начал логгирование

--- LabStudents/main.rb
+++ LabStudents/main.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'views/main_window'
+require './LabStudents/util/logger_holder'
 
+LoggerHolder.instance.level = Logger::DEBUG
 MainWindow.new.create.show
-
